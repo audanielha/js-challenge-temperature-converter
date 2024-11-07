014dc1e0a50e89eb12b9fc66d791df5c14482eb4
Daniel Ha <audanielha@gmail.com>
2024-11-07 22:06:48 +1300

    add remaining converters

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function celsiusToFahrenheit(celsius) {
 function fahrenheitToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5/9;
 }
+function celsiusToKelvin(celsius){
+    return celsius + 273.15;
+}
+function kelvinToCelcius(kelvin){
+    return kelvin - 273.15;
+}
 
   console.log(celsiusToFahrenheit(25)); // Should print 77
   console.log(fahrenheitToCelsius(77)); // Should print 25
+  console.log(celsiusToKelvin(33))
+  console.log(kelvinToCelcius(55))
+
